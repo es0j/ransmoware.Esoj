411af1fd1bb9762f135a0ffc8af125ded32830e7
zekkametallica <37257235+zekkametallica@users.noreply.github.com>
2018-08-11 03:28:29 -0300

    Update ransomsoj.h

--- ransomsoj.h
+++ ransomsoj.h
@@ -1,3 +1,7 @@
+
+
+
+
 #ifndef __list__
 #define __list__ "ransomeware"
 
@@ -5,12 +9,12 @@ int GerarNomes(void);
 
 size_t CriarRandom(int tamanhoBytes);
 
-int CryptBuffer(char *,char *,size_t );
+int CryptBuffer(unsigned long long *,unsigned long long *,size_t );
 
 void b64_encode(char *clrstr, char *b64dst);
 void b64_decode(char *b64src, char *clrdst);
 
 
-int CifrarArquivo(char *,char *chave);
+int CifrarArquivo(char *,unsigned long long *chave);
 
 #endif
